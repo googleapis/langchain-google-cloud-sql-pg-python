b181f658c2e769c74aefc6a53f587ca4a75682db
Averi Kitsch <akitsch@google.com>
2024-02-09 15:50:22 -0800

    feat: add PostgreSQLEngine (#13)
    * feat: add PostgreSQLEngine
    
    * lint
    
    * add header
    
    * Update src/langchain_google_cloud_sql_pg/postgresql_engine.py
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update src/langchain_google_cloud_sql_pg/postgresql_engine.py
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update src/langchain_google_cloud_sql_pg/postgresql_engine.py
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update src/langchain_google_cloud_sql_pg/postgresql_engine.py
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update src/langchain_google_cloud_sql_pg/postgresql_engine.py
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * add json column
    
    * Update comments
    
    * fix
    
    * Update pyproject.toml
    
    * clean up
    
    * Update pyproject.toml
    
    * lint
    
    ---------
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

---
+++ src/langchain_google_cloud_sql_pg/postgresql_engine.py
@@ -0,0 +1,202 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from threading import Thread
+from typing import TYPE_CHECKING, Awaitable, Dict, List, Optional, TypeVar
+
+import aiohttp
+import google.auth  # type: ignore
+import google.auth.transport.requests  # type: ignore
+from google.cloud.sql.connector import Connector, create_async_connector
+from sqlalchemy import text  # Column,
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+if TYPE_CHECKING:
+    import asyncpg  # type: ignore
+    import google.auth.credentials  # type: ignore
+
+T = TypeVar("T")
+
+
+async def _get_iam_principal_email(
+    credentials: google.auth.credentials.Credentials,
+) -> str:
+    """Get email address associated with current authenticated IAM principal.
+
+    Email will be used for automatic IAM database authentication to Cloud SQL.
+
+    Args:
+        credentials (google.auth.credentials.Credentials):
+            The credentials object to use in finding the associated IAM
+            principal email address.
+
+    Returns:
+        email (str):
+            The email address associated with the current authenticated IAM
+            principal.
+    """
+    # refresh credentials if they are not valid
+    if not credentials.valid:
+        request = google.auth.transport.requests.Request()
+        credentials.refresh(request)
+    # call OAuth2 api to get IAM principal email associated with OAuth2 token
+    url = f"https://oauth2.googleapis.com/tokeninfo?access_token={credentials.token}"
+    async with aiohttp.ClientSession() as client:
+        response = await client.get(url, raise_for_status=True)
+        response_json: Dict = await response.json()
+        email = response_json.get("email")
+    if email is None:
+        raise ValueError(
+            "Failed to automatically obtain authenticated IAM princpal's "
+            "email address using environment's ADC credentials!"
+        )
+    return email
+
+
+@dataclass
+class Column:
+    name: str
+    data_type: str
+    nullable: bool = True
+
+
+class PostgreSQLEngine:
+    """A class for managing connections to a Cloud SQL for Postgres database."""
+
+    _connector: Optional[Connector] = None
+
+    def __init__(
+        self,
+        engine: AsyncEngine,
+        loop: Optional[asyncio.AbstractEventLoop],
+        thread: Optional[Thread],
+    ):
+        self._engine = engine
+        self._loop = loop
+        self._thread = thread
+
+    @classmethod
+    def from_instance(
+        cls,
+        project_id: str,
+        region: str,
+        instance: str,
+        database: str,
+    ) -> PostgreSQLEngine:
+        # Running a loop in a background thread allows us to support
+        # async methods from non-async enviroments
+        loop = asyncio.new_event_loop()
+        thread = Thread(target=loop.run_forever, daemon=True)
+        thread.start()
+        coro = cls.afrom_instance(project_id, region, instance, database)
+        return asyncio.run_coroutine_threadsafe(coro, loop).result()
+
+    @classmethod
+    async def _create(
+        cls,
+        project_id: str,
+        region: str,
+        instance: str,
+        database: str,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        thread: Optional[Thread] = None,
+    ) -> PostgreSQLEngine:
+        credentials, _ = google.auth.default(
+            scopes=["https://www.googleapis.com/auth/userinfo.email"]
+        )
+        iam_database_user = await _get_iam_principal_email(credentials)
+        if cls._connector is None:
+            cls._connector = await create_async_connector()
+
+        # anonymous function to be used for SQLAlchemy 'creator' argument
+        def getconn() -> asyncpg.Connection:
+            conn = cls._connector.connect_async(  # type: ignore
+                f"{project_id}:{region}:{instance}",
+                "asyncpg",
+                user=iam_database_user,
+                db=database,
+                enable_iam_auth=True,
+            )
+            return conn
+
+        engine = create_async_engine(
+            "postgresql+asyncpg://",
+            async_creator=getconn,
+        )
+        return cls(engine, loop, thread)
+
+    @classmethod
+    async def afrom_instance(
+        cls,
+        project_id: str,
+        region: str,
+        instance: str,
+        database: str,
+    ) -> PostgreSQLEngine:
+        return await cls._create(project_id, region, instance, database)
+
+    async def _aexecute(self, query: str):
+        """Execute a SQL query."""
+        async with self._engine.connect() as conn:
+            await conn.execute(text(query))
+            await conn.commit()
+
+    async def _afetch(self, query: str):
+        async with self._engine.connect() as conn:
+            """Fetch results from a SQL query."""
+            result = await conn.execute(text(query))
+            result_map = result.mappings()
+            result_fetch = result_map.fetchall()
+
+        return result_fetch
+
+    def run_as_sync(self, coro: Awaitable[T]):  # TODO: add return type
+        if not self._loop:
+            raise Exception("Engine was initialized async.")
+        return asyncio.run_coroutine_threadsafe(coro, self._loop).result()
+
+    async def init_vectorstore_table(
+        self,
+        table_name: str,
+        vector_size: int,
+        content_column: str = "content",
+        embedding_column: str = "embedding",
+        metadata_columns: List[Column] = [],
+        metadata_json_columns: str = "langchain_metadata",
+        id_column: str = "langchain_id",
+        overwrite_existing: bool = False,
+        store_metadata: bool = True,
+    ) -> None:
+        await self._aexecute("CREATE EXTENSION IF NOT EXISTS vector")
+
+        if overwrite_existing:
+            await self._aexecute(f"DROP TABLE IF EXISTS {table_name}")
+
+        query = f"""CREATE TABLE {table_name}(
+            {id_column} UUID PRIMARY KEY,
+            {content_column} TEXT NOT NULL,
+            {embedding_column} vector({vector_size}) NOT NULL"""
+        for column in metadata_columns:
+            query += f",\n{column.name} {column.data_type}" + (
+                "NOT NULL" if not column.nullable else ""
+            )
+        if store_metadata:
+            query += f",\n{metadata_json_columns} JSON"
+        query += "\n);"
+
+        await self._aexecute(query)
---
+++ tests/test_postgresql_engine.py
@@ -0,0 +1,133 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import uuid
+from typing import List
+
+import pytest
+import pytest_asyncio
+from langchain_community.embeddings import FakeEmbeddings
+
+from langchain_google_cloud_sql_pg import Column, PostgreSQLEngine
+
+DEFAULT_TABLE = "test_table" + str(uuid.uuid4()).replace("-", "_")
+CUSTOM_TABLE = "test_table_custom" + str(uuid.uuid4()).replace("-", "_")
+VECTOR_SIZE = 768
+
+
+class FakeEmbeddingsWithDimension(FakeEmbeddings):
+    """Fake embeddings functionality for testing."""
+
+    size: int = VECTOR_SIZE
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        """Return simple embeddings."""
+        return [
+            [float(1.0)] * (VECTOR_SIZE - 1) + [float(i)] for i in range(len(texts))
+        ]
+
+    def embed_query(self, text: str = "default") -> List[float]:
+        """Return simple embeddings."""
+        return [float(1.0)] * (VECTOR_SIZE - 1) + [float(0.0)]
+
+
+embeddings_service = FakeEmbeddingsWithDimension()
+
+
+def get_env_var(key: str, desc: str) -> str:
+    v = os.environ.get(key)
+    if v is None:
+        raise ValueError(f"Must set env var {key} to: {desc}")
+    return v
+
+
+@pytest.mark.asyncio
+class TestEngineAsync:
+    @pytest.fixture(scope="module")
+    def db_project(self) -> str:
+        return get_env_var("PROJECT_ID", "project id for google cloud")
+
+    @pytest.fixture(scope="module")
+    def db_region(self) -> str:
+        return get_env_var("REGION", "region for cloud sql instance")
+
+    @pytest.fixture(scope="module")
+    def db_instance(self) -> str:
+        return get_env_var("INSTANCE_ID", "instance for cloud sql")
+
+    @pytest.fixture(scope="module")
+    def db_name(self) -> str:
+        return get_env_var("DATABASE_ID", "instance for cloud sql")
+
+    @pytest_asyncio.fixture
+    async def engine(self, db_project, db_region, db_instance, db_name):
+        engine = await PostgreSQLEngine.afrom_instance(
+            project_id=db_project,
+            instance=db_instance,
+            region=db_region,
+            database=db_name,
+        )
+        yield engine
+
+    async def test_execute(self, engine):
+        await engine._aexecute("SELECT 1")
+
+    async def test_init_table(self, engine):
+        await engine.init_vectorstore_table(DEFAULT_TABLE, VECTOR_SIZE)
+        id = str(uuid.uuid4())
+        content = "coffee"
+        embedding = await embeddings_service.aembed_query(content)
+        stmt = f"INSERT INTO {DEFAULT_TABLE} (langchain_id, content, embedding) VALUES ('{id}', '{content}','{embedding}');"
+        await engine._aexecute(stmt)
+
+    async def test_fetch(self, engine):
+        results = await engine._afetch(f"SELECT * FROM {DEFAULT_TABLE}")
+        assert len(results) > 0
+        await engine._aexecute(f"DROP TABLE {DEFAULT_TABLE}")
+
+    async def test_init_table_custom(self, engine):
+        await engine.init_vectorstore_table(
+            CUSTOM_TABLE,
+            VECTOR_SIZE,
+            id_column="uuid",
+            content_column="mycontent",
+            embedding_column="myembedding",
+            metadata_columns=[Column("page", "TEXT"), Column("source", "TEXT")],
+            store_metadata=True,
+        )
+        stmt = f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{CUSTOM_TABLE}';"
+        results = await engine._afetch(stmt)
+        expected = [
+            {"column_name": "uuid", "data_type": "uuid"},
+            {"column_name": "myembedding", "data_type": "USER-DEFINED"},
+            {"column_name": "langchain_metadata", "data_type": "json"},
+            {"column_name": "mycontent", "data_type": "text"},
+            {"column_name": "page", "data_type": "text"},
+            {"column_name": "source", "data_type": "text"},
+        ]
+        for row in results:
+            assert row in expected
+
+        await engine._aexecute(f"DROP TABLE {CUSTOM_TABLE}")
+
+    def test_sync_engine(self, db_project, db_region, db_instance, db_name):
+        engine = PostgreSQLEngine.from_instance(
+            project_id=db_project,
+            instance=db_instance,
+            region=db_region,
+            database=db_name,
+        )
+        assert engine
