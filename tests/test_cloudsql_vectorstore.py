4c1fa23ce829b9a927a19dc431bb16f4b4b2a921
Averi Kitsch <akitsch@google.com>
2024-02-13 13:29:08 -0800

    fix

--- tests/test_cloudsql_vectorstore.py
+++ tests/test_cloudsql_vectorstore.py
@@ -20,11 +20,7 @@ import pytest_asyncio
 from langchain_community.embeddings import DeterministicFakeEmbedding
 from langchain_core.documents import Document
 
-from langchain_google_cloud_sql_pg import (
-    CloudSQLVectorStore,
-    Column,
-    PostgreSQLEngine,
-)
+from langchain_google_cloud_sql_pg import CloudSQLVectorStore, Column, PostgreSQLEngine
 
 DEFAULT_TABLE = "test_table" + str(uuid.uuid4()).replace("-", "_")
 DEFAULT_TABLE_SYNC = "test_table_sync" + str(uuid.uuid4()).replace("-", "_")
@@ -34,12 +30,9 @@ VECTOR_SIZE = 768
 embeddings_service = DeterministicFakeEmbedding(size=VECTOR_SIZE)
 
 texts = ["foo", "bar", "baz"]
-metadatas = [
-    {"page": str(i), "source": "google.com"} for i in range(len(texts))
-]
+metadatas = [{"page": str(i), "source": "google.com"} for i in range(len(texts))]
 docs = [
-    Document(page_content=texts[i], metadata=metadatas[i])
-    for i in range(len(texts))
+    Document(page_content=texts[i], metadata=metadatas[i]) for i in range(len(texts))
 ]
 
 embeddings = [embeddings_service.embed_query("foo") for i in range(len(texts))]
@@ -105,7 +98,6 @@ class TestVectorStore:
         engine_sync.run_as_sync(
             engine_sync._aexecute(f"DROP TABLE IF EXISTS {DEFAULT_TABLE_SYNC}")
         )
-        engine_sync.run_as_sync(engine_sync._connector.close_async())
         engine_sync.run_as_sync(engine_sync._engine.dispose())
 
     @pytest_asyncio.fixture(scope="class")
