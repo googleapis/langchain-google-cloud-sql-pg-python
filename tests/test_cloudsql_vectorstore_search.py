5c31d76775cbf22be86260881295245ec4e7bab1
duwenxin99 <duwenxin@google.com>
2024-07-24 16:27:59 -0400

    isort

--- tests/test_cloudsql_vectorstore_search.py
+++ tests/test_cloudsql_vectorstore_search.py
@@ -17,8 +17,8 @@ import uuid
 
 import pytest
 import pytest_asyncio
-from langchain_core.embeddings import DeterministicFakeEmbedding
 from langchain_core.documents import Document
+from langchain_core.embeddings import DeterministicFakeEmbedding
 
 from langchain_google_cloud_sql_pg import Column, PostgresEngine, PostgresVectorStore
 from langchain_google_cloud_sql_pg.indexes import HNSWQueryOptions, IVFFlatQueryOptions
