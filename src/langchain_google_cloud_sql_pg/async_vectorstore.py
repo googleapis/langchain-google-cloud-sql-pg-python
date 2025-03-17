7b7aa1ec8ba006f404695b0a14993c3a5445d590
Disha Prakash <dishaprakash@google.com>
2025-03-17 20:33:02 +0000

    minor change

--- src/langchain_google_cloud_sql_pg/async_vectorstore.py
+++ src/langchain_google_cloud_sql_pg/async_vectorstore.py
@@ -773,7 +773,7 @@ class AsyncPostgresVectorStore(VectorStore):
             documents_with_scores.append(
                 (
                     Document(
-                        page_content=str(ow[self.content_column]),
+                        page_content=str(row[self.content_column]),
                         metadata=metadata,
                         id=row[self.id_column],
                     ),
