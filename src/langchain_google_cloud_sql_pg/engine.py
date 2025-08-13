7917d62c3f9ea2c6ca8ab8d6284cfa2c7e535401
dishaprakash <57954147+dishaprakash@users.noreply.github.com>
2025-08-13 17:57:18 +0000

    refactor!: Refactor PostgresVectorStore and PostgresEngine to depend on PGVectorstore and PGEngine respectively (#316)
    * refactor: Refactor PostgresEngine to depend on PGEngine
    
    * update deps
    
    * update deps
    
    * Linter fix
    
    * fix tests
    
    * linter fix
    
    * refactor!: Refactor PostgresVectorStore to depend on PGVectorstore (#319)
    
    * refactor!: Refactor PostgresVectorStore to depend on PGVectorstore
    
    * Liter fix
    
    * fix tests
    
    * re-expose hybrid search config
    
    * header fix
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * expose hybrid search config through init
    
    ---------
    
    Co-authored-by: Averi Kitsch <akitsch@google.com>

--- src/langchain_google_cloud_sql_pg/engine.py
+++ src/langchain_google_cloud_sql_pg/engine.py
@@ -24,6 +24,7 @@ import aiohttp
 import google.auth  # type: ignore
 import google.auth.transport.requests  # type: ignore
 from google.cloud.sql.connector import Connector, IPTypes, RefreshStrategy
+from langchain_postgres import Column, PGEngine
 from sqlalchemy import MetaData, Table, text
 from sqlalchemy.engine import URL
 from sqlalchemy.exc import InvalidRequestError
@@ -78,58 +79,10 @@ async def _get_iam_principal_email(
     return email.replace(".gserviceaccount.com", "")
 
 
-@dataclass
-class Column:
-    name: str
-    data_type: str
-    nullable: bool = True
-
-    def __post_init__(self):
-        """Check if initialization parameters are valid.
-
-        Raises:
-            ValueError: Raises error if Column name is not string.
-            ValueError: Raises error if data_type is not type string.
-        """
-        if not isinstance(self.name, str):
-            raise ValueError("Column name must be type string")
-        if not isinstance(self.data_type, str):
-            raise ValueError("Column data_type must be type string")
-
-
-class PostgresEngine:
+class PostgresEngine(PGEngine):
     """A class for managing connections to a Cloud SQL for Postgres database."""
 
     _connector: Optional[Connector] = None
-    _default_loop: Optional[asyncio.AbstractEventLoop] = None
-    _default_thread: Optional[Thread] = None
-    __create_key = object()
-
-    def __init__(
-        self,
-        key: object,
-        pool: AsyncEngine,
-        loop: Optional[asyncio.AbstractEventLoop],
-        thread: Optional[Thread],
-    ):
-        """PostgresEngine constructor.
-
-        Args:
-            key (object): Prevent direct constructor usage.
-            pool (AsyncEngine): Async engine connection pool.
-            loop (Optional[asyncio.AbstractEventLoop]): Async event loop used to create the engine.
-            thread (Optional[Thread]): Thread used to create the engine async.
-
-        Raises:
-            Exception: If the constructor is called directly by the user.
-        """
-        if key != PostgresEngine.__create_key:
-            raise Exception(
-                "Only create class through 'create' or 'create_sync' methods!"
-            )
-        self._pool = pool
-        self._loop = loop
-        self._thread = thread
 
     @classmethod
     async def _create(
@@ -219,7 +172,7 @@ class PostgresEngine:
             async_creator=getconn,
             **engine_args,
         )
-        return cls(cls.__create_key, engine, loop, thread)
+        return cls(PGEngine._PGEngine__create_key, engine, loop, thread)  # type: ignore
 
     @classmethod
     def __start_background_loop(
@@ -354,13 +307,22 @@ class PostgresEngine:
         return await asyncio.wrap_future(future)
 
     @classmethod
-    def from_engine(
+    def from_connection_string(
         cls,
-        engine: AsyncEngine,
-        loop: Optional[asyncio.AbstractEventLoop] = None,
+        url: str | URL,
+        **kwargs: Any,
     ) -> PostgresEngine:
-        """Create an PostgresEngine instance from an AsyncEngine."""
-        return cls(cls.__create_key, engine, loop, None)
+        """Create an PostgresEngine instance from arguments. These parameters are pass directly into sqlalchemy's create_async_engine function.
+        Args:
+            url (Union[str | URL]): the URL used to connect to a database
+            **kwargs (Any, optional): sqlalchemy `create_async_engine` arguments
+        Raises:
+            ValueError: If `postgresql+asyncpg` is not specified as the PG driver
+        Returns:
+            PostgresEngine
+        """
+
+        return PostgresEngine.from_engine_args(url=url, **kwargs)
 
     @classmethod
     def from_engine_args(
@@ -396,198 +358,7 @@ class PostgresEngine:
             raise ValueError("Driver must be type 'postgresql+asyncpg'")
 
         engine = create_async_engine(url, **kwargs)
-        return cls(cls.__create_key, engine, cls._default_loop, cls._default_thread)
-
-    async def _run_as_async(self, coro: Awaitable[T]) -> T:
-        """Run an async coroutine asynchronously"""
-        # If a loop has not been provided, attempt to run in current thread
-        if not self._loop:
-            return await coro
-        # Otherwise, run in the background thread
-        return await asyncio.wrap_future(
-            asyncio.run_coroutine_threadsafe(coro, self._loop)
-        )
-
-    def _run_as_sync(self, coro: Awaitable[T]) -> T:
-        """Run an async coroutine synchronously"""
-        if not self._loop:
-            raise Exception(
-                "Engine was initialized without a background loop and cannot call sync methods."
-            )
-        return asyncio.run_coroutine_threadsafe(coro, self._loop).result()
-
-    async def close(self) -> None:
-        """Dispose of connection pool"""
-        await self._run_as_async(self._pool.dispose())
-
-    async def _ainit_vectorstore_table(
-        self,
-        table_name: str,
-        vector_size: int,
-        schema_name: str = "public",
-        content_column: str = "content",
-        embedding_column: str = "embedding",
-        metadata_columns: list[Column] = [],
-        metadata_json_column: str = "langchain_metadata",
-        id_column: Union[str, Column] = "langchain_id",
-        overwrite_existing: bool = False,
-        store_metadata: bool = True,
-    ) -> None:
-        """
-        Create a table for saving of vectors to be used with PostgresVectorStore.
-
-        Args:
-            table_name (str): The Postgres database table name.
-            vector_size (int): Vector size for the embedding model to be used.
-            schema_name (str): The schema name to store Postgres database table.
-                Default: "public".
-            content_column (str): Name of the column to store document content.
-                Default: "page_content".
-            embedding_column (str) : Name of the column to store vector embeddings.
-                Default: "embedding".
-            metadata_columns (list[Column]): A list of Columns to create for custom
-                metadata. Default: []. Optional.
-            metadata_json_column (str): The column to store extra metadata in JSON format.
-                Default: "langchain_metadata". Optional.
-            id_column (Union[str, Column]) : Column to store ids.
-                Default: "langchain_id" column name with data type UUID. Optional.
-            overwrite_existing (bool): Whether to drop existing table. Default: False.
-            store_metadata (bool): Whether to store metadata in the table.
-                Default: True.
-        Raises:
-            :class:`DuplicateTableError <asyncpg.exceptions.DuplicateTableError>`: if table already exists and overwrite flag is not set.
-            :class:`UndefinedObjectError <asyncpg.exceptions.UndefinedObjectError>`: if the data type of the id column is not a postgreSQL data type.
-        """
-        async with self._pool.connect() as conn:
-            await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
-            await conn.commit()
-
-        if overwrite_existing:
-            async with self._pool.connect() as conn:
-                await conn.execute(
-                    text(f'DROP TABLE IF EXISTS "{schema_name}"."{table_name}"')
-                )
-                await conn.commit()
-
-        id_data_type = "UUID" if isinstance(id_column, str) else id_column.data_type
-        id_column_name = id_column if isinstance(id_column, str) else id_column.name
-
-        query = f"""CREATE TABLE "{schema_name}"."{table_name}"(
-            "{id_column_name}" {id_data_type} PRIMARY KEY,
-            "{content_column}" TEXT NOT NULL,
-            "{embedding_column}" vector({vector_size}) NOT NULL"""
-        for column in metadata_columns:
-            nullable = "NOT NULL" if not column.nullable else ""
-            query += f',\n"{column.name}" {column.data_type} {nullable}'
-        if store_metadata:
-            query += f""",\n"{metadata_json_column}" JSON"""
-        query += "\n);"
-
-        async with self._pool.connect() as conn:
-            await conn.execute(text(query))
-            await conn.commit()
-
-    async def ainit_vectorstore_table(
-        self,
-        table_name: str,
-        vector_size: int,
-        schema_name: str = "public",
-        content_column: str = "content",
-        embedding_column: str = "embedding",
-        metadata_columns: list[Column] = [],
-        metadata_json_column: str = "langchain_metadata",
-        id_column: Union[str, Column] = "langchain_id",
-        overwrite_existing: bool = False,
-        store_metadata: bool = True,
-    ) -> None:
-        """
-        Create a table for saving of vectors to be used with PostgresVectorStore.
-
-        Args:
-            table_name (str): The Postgres database table name.
-            vector_size (int): Vector size for the embedding model to be used.
-            schema_name (str): The schema name to store Postgres database table.
-                Default: "public".
-            content_column (str): Name of the column to store document content.
-                Default: "page_content".
-            embedding_column (str) : Name of the column to store vector embeddings.
-                Default: "embedding".
-            metadata_columns (list[Column]): A list of Columns to create for custom
-                metadata. Default: []. Optional.
-            metadata_json_column (str): The column to store extra metadata in JSON format.
-                Default: "langchain_metadata". Optional.
-            id_column (Union[str, Column]) : Column to store ids.
-                Default: "langchain_id" column name with data type UUID. Optional.
-            overwrite_existing (bool): Whether to drop existing table. Default: False.
-            store_metadata (bool): Whether to store metadata in the table.
-                Default: True.
-        """
-        await self._run_as_async(
-            self._ainit_vectorstore_table(
-                table_name,
-                vector_size,
-                schema_name,
-                content_column,
-                embedding_column,
-                metadata_columns,
-                metadata_json_column,
-                id_column,
-                overwrite_existing,
-                store_metadata,
-            )
-        )
-
-    def init_vectorstore_table(
-        self,
-        table_name: str,
-        vector_size: int,
-        schema_name: str = "public",
-        content_column: str = "content",
-        embedding_column: str = "embedding",
-        metadata_columns: list[Column] = [],
-        metadata_json_column: str = "langchain_metadata",
-        id_column: Union[str, Column] = "langchain_id",
-        overwrite_existing: bool = False,
-        store_metadata: bool = True,
-    ) -> None:
-        """
-        Create a table for saving of vectors to be used with PostgresVectorStore.
-
-        Args:
-            table_name (str): The Postgres database table name.
-            vector_size (int): Vector size for the embedding model to be used.
-            schema_name (str): The schema name to store Postgres database table.
-                Default: "public".
-            content_column (str): Name of the column to store document content.
-                Default: "page_content".
-            embedding_column (str) : Name of the column to store vector embeddings.
-                Default: "embedding".
-            metadata_columns (list[Column]): A list of Columns to create for custom
-                metadata. Default: []. Optional.
-            metadata_json_column (str): The column to store extra metadata in JSON format.
-                Default: "langchain_metadata". Optional.
-            id_column (Union[str, Column]) : Column to store ids.
-                Default: "langchain_id" column name with data type UUID. Optional.
-            overwrite_existing (bool): Whether to drop existing table. Default: False.
-            store_metadata (bool): Whether to store metadata in the table.
-                Default: True.
-        Raises:
-            :class:`UndefinedObjectError <asyncpg.exceptions.UndefinedObjectError>`: if the `ids` data type does not match that of the `id_column`.
-        """
-        self._run_as_sync(
-            self._ainit_vectorstore_table(
-                table_name,
-                vector_size,
-                schema_name,
-                content_column,
-                embedding_column,
-                metadata_columns,
-                metadata_json_column,
-                id_column,
-                overwrite_existing,
-                store_metadata,
-            )
-        )
+        return cls(PGEngine._PGEngine__create_key, engine, cls._default_loop, cls._default_thread)  # type: ignore
 
     async def _ainit_chat_history_table(
         self, table_name: str, schema_name: str = "public"
