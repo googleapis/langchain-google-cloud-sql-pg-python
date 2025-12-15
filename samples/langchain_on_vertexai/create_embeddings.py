e373c3d86f5d6be4141d4516a5c367ecd4153444
dishaprakash <57954147+dishaprakash@users.noreply.github.com>
2025-12-15 22:35:50 +0530

    Update create_embeddings.py

--- samples/langchain_on_vertexai/create_embeddings.py
+++ samples/langchain_on_vertexai/create_embeddings.py
@@ -31,6 +31,7 @@ from sqlalchemy import text
 
 from langchain_google_cloud_sql_pg import PostgresEngine, PostgresVectorStore
 
+
 async def run_on_background(engine: PostgresEngine, coro: Coroutine) -> Any:
     """Runs a coroutine on the engine's background loop."""
     if engine._default_loop:
@@ -38,6 +39,7 @@ async def run_on_background(engine: PostgresEngine, coro: Coroutine) -> Any:
             asyncio.run_coroutine_threadsafe(coro, engine._default_loop)
         )
     return await coro
+
 
 async def create_databases():
     engine = await PostgresEngine.afrom_instance(
@@ -48,11 +50,13 @@ async def create_databases():
         user=USER,
         password=PASSWORD,
     )
+
     async def _logic():
         async with engine._pool.connect() as conn:
             await conn.execute(text("COMMIT"))
             await conn.execute(text(f'DROP DATABASE IF EXISTS "{DATABASE}"'))
             await conn.execute(text(f'CREATE DATABASE "{DATABASE}"'))
+
     await run_on_background(engine, _logic())
     await engine.close()
 
